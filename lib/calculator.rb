b8ded12dd8c850cd2d132cbd5d1c7bc24bb8568a
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-10-31 11:54:52 +0000

    Added unit test and code for square feature

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,8 @@ class Calculator
   def multiply(a, b)
     a * b
   end
+
+  def square(a)
+    multiply(a, a)
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,6 +2,11 @@ require './lib/calculator'
 describe Calculator do
   it 'multiplies two numbers' do
     calculator = Calculator.new
-    expect(calculator.multiply(1, 1)).to eq(1)
+    expect(subject.multiply(1, 1)).to eq(1)
+  end
+
+  it 'squares a number' do
+    calculator = Calculator.new
+    expect(subject.square(2)).to eq(4)
   end
 end
